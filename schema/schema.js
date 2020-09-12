0b21c5db3b188f667247a7b54eb1a1938b51132a
David van Zyl <davidvanzyl@gmail.com>
2020-09-12 13:49:02 +0200

    RootQuery initial setup

--- schema/schema.js
+++ schema/schema.js
@@ -13,3 +13,20 @@ const UserType = new GraphQLObjectType({
         age: {type: GraphQLInt},
     }
 })
+
+// the RootQuery will allow GraphQL to 'jump and land' on a very specific
+// node in the graph of all of our data
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            // the resolve function is where we go in to the db
+            // and find/return data we are looking for
+            resolve(parentValue, args){
+
+            }
+        }
+    }
+})
