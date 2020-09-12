9f8299910068eb633b143ec4c0174f5d366e3db3
David van Zyl <davidvanzyl@gmail.com>
2020-09-12 15:04:45 +0200

    Linked users to associated company

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,28 @@ const {
     GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+    }
+})
+
 const UserType = new GraphQLObjectType({
     name: 'User', // note: by convention, capitalize named type
     fields: { // tells GraphQL all the properties a user has and their types
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
         age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(res => res.data);
+            }
+        }
     }
 })
 
@@ -26,7 +42,7 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
             // the resolve function is where we go in to the db
             //  and find/return data we are looking for
-            resolve(parentValue, args){
+            resolve(parentValue, args) {
                 // graphql handles waiting for an async promise to resolve
                 return axios.get(`http://localhost:3000/users/${args.id}`)
                     // gotcha with axios {data: <actual response>}
